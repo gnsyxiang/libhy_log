3e37723f04f9e8ccb00ad7c2a7f33ad90b2b7dec
zhenquan.qiu <gnsyxiang@163.com>
2023-08-10 16:47:40 +0800

    refactor: refactor dynamic_array.c

--- src/hy_log/dynamic_array.c
+++ src/hy_log/dynamic_array.c
@@ -27,95 +27,89 @@
 #define HY_MEM_ALIGN4(len)          HY_MEM_ALIGN(len, 4)
 #define HY_MEM_ALIGN4_UP(len)       (HY_MEM_ALIGN(len, 4) + HY_MEM_ALIGN4(1))
 
-int32_t dynamic_array_read(dynamic_array_s *context,
-        void *buf, uint32_t len)
+static int32_t _dynamic_array_extend(dynamic_array_s *handle, uint32_t increment)
 {
-    assert(context);
-    assert(buf);
+    assert(handle);
 
-    if (len > context->cur_len) {
-        len = context->cur_len;
-    }
-
-    memcpy(buf, context->buf + context->read_pos, len);
-    context->read_pos += len;
-    context->cur_len  -= len;
-
-    return len;
-}
-
-static int32_t _dynamic_array_extend(dynamic_array_s *context,
-        uint32_t increment)
-{
-    assert(context);
     uint32_t extend_len = 0;
     int32_t ret = 0;
     void *ptr = NULL;
 
-    if (context->len >= context->max_len) {
-        log_e("can't extend size, len: %d, max_len: %d \n",
-                context->len, context->max_len);
+    if (handle->len >= handle->max_len) {
+        log_e("can't extend, len: %d, max_len: %d \n", handle->len, handle->max_len);
         return -1;
     }
 
-    if (context->len + increment <= context->max_len) {
-        extend_len = HY_MEM_ALIGN4_UP(context->len + increment + 1);
+    if (handle->len + increment <= handle->max_len) {
+        extend_len = HY_MEM_ALIGN4_UP(handle->len + increment + 1);
     } else {
-        extend_len = context->max_len;
-        ret = 1;
-        log_e("extend to max len, len: %d, extend_len: %d \n",
-                context->len, extend_len);
+        extend_len = handle->max_len;
+        ret = 1; // 已经达到分配空间最大值，给被调用函数做截断处理提供条件
+        log_e("extend to max len, len: %d, extend_len: %d \n", handle->len, extend_len);
     }
 
-    ptr = realloc(context->buf, extend_len);
+    ptr = realloc(handle->buf, extend_len);
     if (!ptr) {
         log_e("realloc failed \n");
         return -1;
     }
 
-    context->buf = ptr;
-    context->len = extend_len;
+    handle->buf = ptr;
+    handle->len = extend_len;
 
     return ret;
 }
 
-int32_t dynamic_array_write_vprintf(dynamic_array_s *context,
-        const char *format, va_list *args)
+int32_t dynamic_array_read(dynamic_array_s *handle, void *buf, uint32_t len)
 {
-    assert(context);
+    assert(handle);
+    assert(buf);
+
+    if (len > handle->cur_len) {
+        len = handle->cur_len;
+    }
+
+    memcpy(buf, handle->buf + handle->read_pos, len);
+    handle->read_pos += len;
+    handle->cur_len  -= len;
+
+    return len;
+}
+
+int32_t dynamic_array_write_vprintf(dynamic_array_s *handle,
+                                    const char *format, va_list *args)
+{
+    assert(handle);
     assert(format);
     assert(args);
-    #define _get_dynamic_info()                                 \
-        do {                                                    \
-            va_copy(ap, *args);                                 \
-            free_len = context->len - context->cur_len - 1;     \
-            ptr = context->buf + context->write_pos;            \
-        } while (0)
 
-    va_list ap;
     int32_t free_len;
     int32_t ret;
     char *ptr = NULL;
 
-    _get_dynamic_info();
-    ret = vsnprintf(ptr, free_len, format, ap);
+    ptr = handle->buf + handle->write_pos;
+
+    free_len = handle->len - handle->cur_len - 1; // -1为'\0'保留
+
+    ret = vsnprintf(ptr, free_len, format, *args);
     if (ret < 0) {
         log_e("vsnprintf failed \n");
         ret = -1;
     } else if (ret >= 0) {
         if (ret < free_len) {
-            context->cur_len += ret;
-            context->write_pos += ret;
+            handle->cur_len   += ret;
+            handle->write_pos += ret;
         } else {
-            ret = _dynamic_array_extend(context,
-                    ret - (context->len - context->cur_len));
+            ret = _dynamic_array_extend(handle, ret - (handle->len - handle->cur_len));
             if (-1 == ret) {
                 log_i("_dynamic_array_extend failed \n");
             } else if (ret >= 0) {
-                _get_dynamic_info();
-                ret = vsnprintf(ptr, free_len, format, ap);
-                context->cur_len += ret;
-                context->write_pos += ret;
+                // 扩大内存后，重新计算空闲空间
+                free_len = handle->len - handle->cur_len - 1; // -1为'\0'保留
+
+                ret = vsnprintf(ptr, free_len, format, *args);
+                handle->cur_len += ret;
+                handle->write_pos += ret;
 
                 /* @fixme: <22-04-23, uos> 做进一步判断 */
             }
@@ -125,27 +119,26 @@ int32_t dynamic_array_write_vprintf(dynamic_array_s *context,
     return ret;
 }
 
-int32_t dynamic_array_write(dynamic_array_s *context,
-        const void *buf, uint32_t len)
+int32_t dynamic_array_write(dynamic_array_s *handle, const void *buf, uint32_t len)
 {
     #define _write_data_com(_buf, _len)                 \
         do {                                            \
             char *ptr = NULL;                           \
-            ptr = context->buf + context->write_pos;    \
+            ptr = handle->buf + handle->write_pos;      \
             memcpy(ptr, _buf, _len);                    \
-            context->cur_len      += _len;              \
-            context->write_pos    += _len;              \
+            handle->cur_len      += _len;               \
+            handle->write_pos    += _len;               \
         } while (0)
 
-    assert(context);
+    assert(handle);
     assert(buf);
+
     int32_t ret = 0;
 
-    if (context->len - context->cur_len > len) {
+    if (handle->len > handle->cur_len && handle->len - handle->cur_len > len) {
         _write_data_com(buf, len);
     } else {
-        ret = _dynamic_array_extend(context,
-                len - (context->len - context->cur_len));
+        ret = _dynamic_array_extend(handle, len - (handle->len - handle->cur_len));
         if (-1 == ret) {
             log_i("_dynamic_array_extend failed \n");
             len = -1;
@@ -153,10 +146,10 @@ int32_t dynamic_array_write(dynamic_array_s *context,
             _write_data_com(buf, len);
         } else {
             // 3 for "..." 1 for "\0"
-            len = context->len - context->cur_len - 3 - 1;
+            len = handle->len - handle->cur_len - 3 - 1;
             _write_data_com(buf, len);
 
-            log_i("truncated data \n");
+            log_e("truncated data \n");
             _write_data_com("...", 3);
             len += 3;
         }
@@ -165,20 +158,19 @@ int32_t dynamic_array_write(dynamic_array_s *context,
     return len;
 }
 
-void dynamic_array_destroy(dynamic_array_s **context_pp)
+void dynamic_array_destroy(dynamic_array_s **handle_pp)
 {
-    dynamic_array_s *context = *context_pp;
-
-    if (!context_pp || !*context_pp) {
+    if (!handle_pp || !*handle_pp) {
         log_e("the param is error \n");
         return;
     }
-    log_i("dynamic array context: %p destroy, buf: %p \n",
-            context, context->buf);
+    dynamic_array_s *handle = *handle_pp;
 
-    free(context->buf);
-    free(context);
-    *context_pp = NULL;
+    log_i("dynamic array handle: %p destroy, buf: %p \n", handle, handle->buf);
+
+    free(handle->buf);
+    free(handle);
+    *handle_pp = NULL;
 }
 
 dynamic_array_s *dynamic_array_create(uint32_t min_len, uint32_t max_len)
@@ -188,32 +180,30 @@ dynamic_array_s *dynamic_array_create(uint32_t min_len, uint32_t max_len)
         return NULL;
     }
 
-    dynamic_array_s *context = NULL;
+    dynamic_array_s *handle = NULL;
     do {
-        context = calloc(1, sizeof(*context));
-        if (!context) {
+        handle = calloc(1, sizeof(*handle));
+        if (!handle) {
             log_e("calloc failed \n");
             break;
         }
 
-        context->buf =calloc(1, min_len);
-        if (!context->buf) {
+        handle->buf =calloc(1, min_len);
+        if (!handle->buf) {
             log_e("calloc failed \n");
             break;
         }
 
-        context->max_len    = max_len;
-        context->min_len    = min_len;
-        context->len        = context->min_len;
-        context->write_pos  = context->read_pos = 0;
+        handle->max_len    = max_len;
+        handle->min_len    = min_len;
+        handle->len        = handle->min_len;
+        handle->write_pos  = handle->read_pos = 0;
 
-        log_i("dynamic array context: %p create, buf: %p \n",
-                context, context->buf);
-        return context;
+        log_i("dynamic array handle: %p create, buf: %p \n", handle, handle->buf);
+        return handle;
     } while (0);
 
-    log_e("dynamic array context: %p create failed \n", context);
-    dynamic_array_destroy(&context);
+    log_e("dynamic array handle: %p create failed \n", handle);
+    dynamic_array_destroy(&handle);
     return NULL;
 }
-
